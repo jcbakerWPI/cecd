467f158db4197628de09e3339b677d64312c8095
Jackson <jcbaker@wpi.edu>
2017-12-10 21:44:19 +0200

    final styling changes

--- server.js
+++ server.js
@@ -34,24 +34,7 @@ var server = http.createServer (function (req, res) {
     case '/images/ct.png':
       sendFile(res, 'public/images/ct.png', 'image/ppng')
       break
-    case '/images/culture.jpg':
-      sendFile(res, 'public/images/culture.jpg', 'image/jpg')
-      break
-    case '/images/good-kid.jpg':
-      sendFile(res, 'public/images/good-kid.jpg', 'image/jpg')
-      break
-    case '/images/orange.jpg':
-      sendFile(res, 'public/images/orange.jpg', 'image/jpg')
-      break
-    case '/images/tile.jpg':
-      sendFile(res, 'public/images/tile.jpg', 'image/jpg')
-      break
-    case '/images/water.jpg':
-      sendFile(res, 'public/images/water.jpg', 'image/jpg')
-      break
-    case '/images/wave.jpg':
-      sendFile(res, 'public/images/wave.jpg', 'image/jpg')
-      break
+  
     
     default:
       res.end('404 not found')
